4363ccf09a17818dc2db920cd3f1ae93016ff62d
greszter <esztergreskovics@gmail.com>
2020-12-17 14:16:44 +0100

    add button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 
+import StyledButton from '../StyledButton';
 import ModelX from '../../assets/ModelX.jpeg';
 import styles from './styles';
 
 const CarItem = () => {
   return (
     <View style={styles.carContainer}>
-    <ImageBackground 
-      style={styles.image}
-      source={ModelX}
+      <ImageBackground 
+        style={styles.image}
+        source={ModelX}
       />
       <View style={styles.titles}>
-      <Text style={styles.title}>Model S</Text>
-      <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+      <StyledButton type="primary" content={'Custom Order'} onPress={() => console.warn('custom order pressed')} />
+      <StyledButton type="secondary" content={'Existing Inventory'} onPress={() => console.warn('inventory pressed')} />
     </View>
   )
 }
